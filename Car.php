3145fba55f2ebe86125613167a7123d2a9b29419
Nathan Chapelle <nathanchapelle76@gmail.com>
2021-10-26 23:59:15 +0200

    added try catch

--- Car.php
+++ Car.php
@@ -8,7 +8,7 @@ class Car extends Vehicle
     protected string $energy;
     protected int $energyLevel = 100;
     protected bool $isEngineOn = false;
-
+    private bool $hasParkBrake = true;
     public function __construct(string $color, int $nbSeats, string $energy)
     {
         parent::__construct($color, $nbSeats);
@@ -19,6 +19,9 @@ class Car extends Vehicle
 
     public function start(): string
     {
+        if ($this->getHasParkBrake()) {
+            throw new Exception("This car is parked");
+        }
         if ($this->isEngineOn) {
             return 'Engine is already On';
         } else {
@@ -57,5 +60,15 @@ class Car extends Vehicle
     public function setIsEngineOn($isEngineOn): void
     {
         $this->isEngineOn = $isEngineOn;
+    }
+
+    public function setHasParkBrake(bool $hasParkBrake)
+    {
+        $this->hasParkBrake = $hasParkBrake;
+    }
+
+    public function getHasParkBrake(): bool
+    {
+        return $this->hasParkBrake;
     }
 }
